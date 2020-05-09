86ede3a4213c299c68628c3da0cc4901bfa1b71b
TS.SZX.chenwenwu <chenwenwu@travelsky.com>
2020-05-09 10:18:38 +0800

    initial

---
+++ src/main/java/com/vidor/SpringbootmodulesApplication.java
@@ -0,0 +1,13 @@
+package com.vidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmodulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmodulesApplication.class, args);
+    }
+
+}
